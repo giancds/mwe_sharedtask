d65459d4f5b144f480529cf45fb60a830e078a02
Giancarlo Salton <giancds@gmail.com>
2020-05-29 18:46:46 -0300

    fixed points_to_evaluate

--- transformer_nn_classifier.py
+++ transformer_nn_classifier.py
@@ -64,9 +64,9 @@ def train_model(config):
     model = tf.keras.Sequential()
     # embedding
 
-    layer_config = ([config["layer_size"]] * config["nlayers"]
-                    if config["nlayers"] > 0 and config["layer_size"] > 0
-                    else config["layers"])
+    layer_config = ([config["layer_size"]] *
+                    config["nlayers"] if config["nlayers"] > 0 and
+                    config["layer_size"] > 0 else config["layers"])
 
     # Dense layers
     for i, layer_size in enumerate(layer_config):
@@ -170,17 +170,28 @@ def train_model(config):
 
 
 search_space = {
-    "dropout": hp.uniform("dropout", 0.0, 0.9),
-    "max_epochs": hp.choice("max_epochs", [10, 20, 30, 50]),
-    "early_stop_delta": hp.choice("early_stop_delta", [0.001, 0.0001]),
-    "early_stop_patience": hp.choice("early_stop_patience", [10, 20]),
-    "hidden_activation": hp.choice("hidden_activation", ['tanh', 'relu', 'elu', 'selu']),
-    "output_activation": hp.choice("output_activation", ['sigmoid', 'softmax']),
-    "clipnorm": hp.choice("clipnorm", [0.5, 1.0, 2.5, 5.0, 10.0]),
-    "learning_rate": hp.loguniform("learning_rate", np.log(1e-4), np.log(1e-0)),
-    "batch_size": hp.choice("batch_size", [20, 24, 32, 64, 128]),
-    "n_layers": hp.randint('n_layer', 1, 5) * 1,
-    "layer_size": hp.randint('layer_size', 1, 100) * 10,
+    "dropout":
+        hp.uniform("dropout", 0.0, 0.9),
+    "max_epochs":
+        hp.choice("max_epochs", [10, 20, 30, 50]),
+    "early_stop_delta":
+        hp.choice("early_stop_delta", [0.001, 0.0001]),
+    "early_stop_patience":
+        hp.choice("early_stop_patience", [10, 20]),
+    "hidden_activation":
+        hp.choice("hidden_activation", ['tanh', 'relu', 'elu', 'selu']),
+    "output_activation":
+        hp.choice("output_activation", ['sigmoid', 'softmax']),
+    "clipnorm":
+        hp.choice("clipnorm", [0.5, 1.0, 2.5, 5.0, 10.0]),
+    "learning_rate":
+        hp.loguniform("learning_rate", np.log(1e-4), np.log(1e-0)),
+    "batch_size":
+        hp.choice("batch_size", [20, 24, 32, 64, 128]),
+    "nlayers":
+        hp.randint('nlayers', 1, 5) * 1,
+    "layer_size":
+        hp.randint('layer_size', 1, 100) * 10,
 }
 
 _config.update({
@@ -188,9 +199,8 @@ _config.update({
     "optimizer": 'adam',
     "threads": 4,
     "output_size": 2,
-    "num_samples": 1
+    # "num_samples": 2
 })
-
 
 if not _config["tune"]:
 
@@ -198,53 +208,49 @@ if not _config["tune"]:
 
 else:
 
-
     reporter = tune.CLIReporter()
     reporter.add_metric_column('keras_info/label1_f1_score', 'f1-score')
 
-    ray.shutdown()     # Restart Ray defensively in case the ray connection is lost.
+    ray.shutdown(
+    )     # Restart Ray defensively in case the ray connection is lost.
     ray.init(num_cpus=6)
-    results = tune.run(
-        train_model,
-        name="tune-nn-bert-classifier",
-        config=_config,
-        stop={
-            "keras_info/f1_score": 0.99,
-            "training_iteration": 10**8
-        },
-        resources_per_trial={
-            "cpu": 1,
-            "gpu": 0
-        },
-        num_samples=_config["num_samples"],
-        checkpoint_freq=0,
-        checkpoint_at_end=False,
-        scheduler=AsyncHyperBandScheduler(
-            time_attr='epoch',
-            metric='f1_score',
-            mode='max',
-            max_t=400,
-            grace_period=20),
-        search_alg=HyperOptSearch(
-            search_space,
-            metric="keras_info/f1_score",
-            mode="max",
-            random_state_seed=SEED,
-            points_to_evaluate=[{
-                "dropout": 0.2,
-                "max_epochs": 2,
-                "early_stop_delta": 0,
-                "early_stop_patience": 0,
-                "hidden_activation": 1,
-                "output_activation": 0,
-                "clipnorm": 3,
-                "learning_rate": 0.0001,
-                "batch_size": 3,
-                "nlayers": 2,
-                "layer_size": 100
-            }]),
-        progress_reporter=reporter,
-        verbose=1)
-    results.dataframe().to_csv(
-        '{0}/nn_results{1}layers.csv'.format(
-            _config["train_dir"], _config['layers']))
+    results = tune.run(train_model,
+                       name="tune-nn-bert-classifier",
+                       config=_config,
+                       stop={
+                           "keras_info/f1_score": 0.99,
+                           "training_iteration": 10**8
+                       },
+                       resources_per_trial={
+                           "cpu": 1,
+                           "gpu": 0
+                       },
+                       num_samples=_config["num_samples"],
+                       checkpoint_freq=0,
+                       checkpoint_at_end=False,
+                       scheduler=AsyncHyperBandScheduler(time_attr='epoch',
+                                                         metric='f1_score',
+                                                         mode='max',
+                                                         max_t=400,
+                                                         grace_period=20),
+                       search_alg=HyperOptSearch(search_space,
+                                                 metric="keras_info/f1_score",
+                                                 mode="max",
+                                                 random_state_seed=SEED,
+                                                 points_to_evaluate=[{
+                                                     "dropout": 0.2,
+                                                     "max_epochs": 2,
+                                                     "early_stop_delta": 0,
+                                                     "early_stop_patience": 0,
+                                                     "hidden_activation": 1,
+                                                     "output_activation": 0,
+                                                     "clipnorm": 3,
+                                                     "learning_rate": 0.0001,
+                                                     "batch_size": 3,
+                                                     "nlayers": 2,
+                                                     "layer_size": 100
+                                                 }]),
+                       progress_reporter=reporter,
+                       verbose=1)
+    results.dataframe().to_csv('{0}/nn_results{1}layers.csv'.format(
+        _config["train_dir"], _config['layers']))
