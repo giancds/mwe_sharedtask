9f6cc8ec0e381237d9a396053e81b3ba75edf8a1
Giancarlo Salton <giancds@gmail.com>
2020-05-30 16:53:21 -0300

    fixed the tune run

--- transformer_sent_classifier.py
+++ transformer_sent_classifier.py
@@ -17,8 +17,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import HistGradientBoostingClassifier
 
-from skopt import BayesSearchCV
-from skopt.space import Real, Categorical, Integer
+import ray
+# from ray import tune
+from ray.tune.schedulers import AsyncHyperBandScheduler
+from ray.tune.suggest.hyperopt import HyperOptSearch
+from hyperopt import hp
 
 SEED = 42
 BASE_DIR = os.path.expanduser("~")     # this will point to the user's home
@@ -44,94 +47,145 @@ print(x_dev.shape, y_dev.shape)
 del data
 
 
+def run_tune(fit_function, space):
+
+    results = ray.tune.run(
+        fit_function,
+        name="tune-nn-bert-classifier-lr",
+        config={},
+        stop={"cv_score/f1-mean": 0.90,},
+        num_samples=10,
+        scheduler=AsyncHyperBandScheduler(
+            time_attr='training_iteration',
+            metric="cv_score/f1-mean",
+            mode='max'),
+        search_alg=HyperOptSearch(
+            space,
+            metric="cv_score/f1-mean",
+            mode="max",
+            random_state_seed=SEED),
+        verbose=1)
+    df = results.dataframe()
+    try:
+        df0 = pd.read_csv('{}/results.csv'.format(TRAIN_DIR))
+        df = pd.concat([df0, df])
+    except FileNotFoundError as e:
+        print(e)
+    df.to_csv('{}/results.csv'.format(TRAIN_DIR))
+
+
+def fit_and_score(clf):
+
+    scores = cross_val_score(
+        clf, x_train, y_train, cv=3, scoring='f1')
+
+    score_log = {'f1-mean': scores.mean()}
+    for i, score in enumerate(scores):
+        score_log['f1-cv{}'.format(i+1)] = score
+
+    ray.tune.track.log(cv_score=score_log)
+
+
+def fit_knn(config):
+    fit_and_score(KNeighborsClassifier(**config))
+
+
+def fit_linear_svm(config):
+    fit_and_score(LinearSVC(**config))
+
+
+def fit_svm(config):
+    fit_and_score(SVC(**config))
+
+
+def fit_lregression(config):
+    fit_and_score(LogisticRegression(**config))
+
+
+def fit_adaboost(config):
+    fit_and_score(AdaBoostClassifier(**config))
+
+
+def fit_histgrad(config):
+    fit_and_score(HistGradientBoostingClassifier(**config))
+
+
+def fit_rforest(config):
+    fit_and_score(RandomForestClassifier(**config))
+
+
 knn_space = {
-    'model': [KNeighborsClassifier(n_jobs=-1)],
-    'model__n_neighbors': Integer(1, 10),
-    'model__weights':  Categorical(['uniform', 'distance']),
-    'model__p':  Integer(1, 2)
+    'n_neighbors': hp.randint('n_neighbors', 1, 10),
+    'weights':  hp.choice('weights', ['uniform', 'distance']),
+    'p':  hp.randint('p', 1, 2) * 1,
+    'n_jobs': hp.choice('n_jobs', [-1])
 }
 
 lsvm_space = {
-    'model': [LinearSVC(dual=False, class_weight='balanced', random_state=SEED)],
-    'model__penalty': Categorical(['l1', 'l2']),
-    'model__loss':  Categorical(['hinge', 'squared_hinge']),
-    'model__C': Real(1e-6, 1e+6, prior='log-uniform'),
+    'penalty':hp.choice('penalty', ['l1', 'l2']),
+    'loss':  hp.choice('loss', ['hinge', 'squared_hinge']),
+    'C': hp.loguniform('C', np.log(1e-6), np.log(1e+4)),
+    'class_weight': hp.choice('class_weight', ['balanced']),
+    'random_state': hp.choice('random_state', [SEED]),
+    'dual': hp.choice('dual', [True, False])
 }
 
-
 svm_space = {
-    'model': [SVC(class_weight='balanced', random_state=SEED)],
-    'model__kernel': Categorical(['linear', 'poly', 'rbf', 'sigmoid']),
-    'model__degree': Integer(1, 3),
-    'model__gamma': Categorical(['scale', 'auto']),
-    'model__C': Real(1e-6, 1e+6, prior='log-uniform'),
+    'kernel': hp.choice('kernel', ['linear', 'poly', 'rbf', 'sigmoid']),
+    'degree': hp.randint('degree', 1, 3) * 1,
+    'gamma': hp.choice('gamma', ['scale', 'auto']),
+    'C': hp.loguniform('C', np.log(1e-6), np.log(1e+4)),
+    'class_weight': hp.choice('class_weight', ['balanced']),
+    'random_state': hp.choice('random_state', [SEED]),
 }
 
 lreg_space = {
-    'model': [LogisticRegression(solver='liblinear')],
-    'model__penalty': Categorical(['l1', 'l2']),
-    'model__C': Real(1e-6, 1e+6, prior='log-uniform'),
+    'penalty': hp.choice('penalty', ['l1', 'l2']),
+    'C': hp.loguniform('C', np.log(1e-6), np.log(1e+4)),
+    'solver': hp.choice('solver', ['liblinear']),
+    'n_jobs': hp.choice('n_jobs', [-1])
 }
 
 adab_space = {
-    'model': [AdaBoostClassifier(
-        DecisionTreeClassifier(max_depth=Integer(1, 10)),
-        random_state=SEED)],
-    'model__n_estimators': Integer(5, 50) ,
-    'model__learning_rate': Real(0.1, 1.0, prior='uniform'),
+    'base_estimator': hp.choice('base_estimator', [DecisionTreeClassifier(max_depth=hp.randint('max_depth', 1, 10))]),
+    'n_estimators': hp.randint('n_estimators', 1, 100) * 5,
+    'learning_rate': hp.uniform('learning_rate', 0.1, 1.0),
+    'random_state': hp.choice('random_state', [SEED]),
 }
 
 hist_space = {
-    'model': [HistGradientBoostingClassifier(random_state=SEED)],
-    'model__learning_rate': Real(0.1, 1.0, prior='uniform'),
-    'model__max_leaf_nodes': Integer(2, 100),
-    'model__max_depth': Integer(2, 100),
-    'model__min_samples_leaf': Integer(1, 50),
-    'model__l2_regularization': Real(0.0, 10.0, prior='uniform'),
-    'model__max_bins': Integer(5, 300)
+    'learning_rate': hp.uniform('learning_rate', 0.1, 1.0),
+    'max_leaf_nodes': hp.randint('max_leaf_nodes', 2, 100) * 1,
+    'max_depth': hp.randint('max_depth', 2, 100) * 1,
+    'min_samples_leaf': hp.randint('min_samples_leaf', 1, 50) * 1,
+    'l2_regularization': hp.uniform('l2_regularization', 0.0, 10.0),
+    'max_bins': hp.randint('max_bins', 1, 60) * 5,
+    'random_state': hp.choice('random_state', [SEED]),
 }
 
 rf_space = {
-    'model': [RandomForestClassifier(random_state=SEED, n_jobs=-1)],
-    'model__n_estimators': Integer(5, 500),
-    'model__criterion': Categorical(['gini', 'entropy']),
-    'model__max_leaf_nodes': Integer(2, 100),
-    'model__max_depth': Integer(2, 100),
-    'model__min_samples_leaf': Integer(1, 50),
-    'model__min_samples_split': Integer(1, 10),
-    'model__max_features': Categorical(['auto', 'sqrt', 'log2', None]),
-    'model__l2_regularization': Real(0.0, 10.0, prior='uniform')
+    'n_estimators': hp.randint('n_estimators', 1, 100) * 5,
+    'criterion': hp.choice('criterion', ['gini', 'entropy']),
+    'max_leaf_nodes': hp.randint('max_leaf_nodes', 2, 100) * 1,
+    'max_depth': hp.randint('max_depth', 2, 100) * 1,
+    'min_samples_leaf': hp.randint('min_samples_leaf', 1, 50) * 1,
+    'min_samples_split': hp.randint('min_samples_split', 1, 10) * 1,
+    'max_features': hp.choice('max_features', ['auto', 'sqrt', 'log2', None]),
+    'random_state': hp.choice('random_state', [SEED]),
+    'n_jobs': hp.choice('n_jobs', [-1])
 }
 
-
-pipe = Pipeline([
-    ('model', LogisticRegression(solver='liblinear'))
-])
-
-opt = BayesSearchCV(
-   pipe,[
-       (knn_space, 20),
-       (lsvm_space, 20),
-       (svm_space, 20),
-       (lreg_space, 20),
-       (adab_space, 20),
-       (hist_space, 20),
-       (rf_space, 20),
-     ], # (parameter space, # of evaluations)
-    n_iter=32,
-    cv=3,
-    n_jobs=-1,
-    random_state=SEED,
-    verbose=2,
-    scoring='f1'
-)
-opt.fit(x_train, y_train)
+ray.shutdown()
+ray.init()
 
 
-print("val. score: %s" % opt.best_score_)
-print("test score: %s" % opt.score(x_dev, y_dev))
+run_tune(fit_knn, knn_space)
+run_tune(fit_linear_svm, lsvm_space)
+run_tune(fit_svm, svm_space)
+run_tune(fit_lregression, lreg_space)
+run_tune(fit_adaboost, adab_space)
+run_tune(fit_histgrad, hist_space)
+run_tune(fit_rforest, rf_space)
 
-y_pred = opt.predict(x_dev)
 
-print(confusion_matrix(y_dev, y_pred))
-print(classification_report(y_dev, y_pred))
+ray.shutdown()
